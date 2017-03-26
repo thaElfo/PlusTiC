15d62b7f50ce1146375123957c10c00d870921e4
Thanh Tran <python0429@gmail.com>
2017-03-25 20:48:36 -0400

    Bump version

--- src/main/java/landmaster/plustic/PlusTiC.java
+++ src/main/java/landmaster/plustic/PlusTiC.java
@@ -36,7 +36,7 @@ import static slimeknights.tconstruct.tools.TinkerTraits.*;
 public class PlusTiC {
 	public static final String MODID = "plustic";
 	public static final String NAME = "PlusTiC";
-	public static final String VERSION = "3.1";
+	public static final String VERSION = "4.0";
 	public static final String DEPENDS = "required-after:mantle;required-after:tconstruct;after:Mekanism;after:BiomesOPlenty;after:Botania;after:advancedRocketry;after:armorplus;after:EnderIO;after:projectred-exploration;after:thermalfoundation;after:draconicevolution;after:landcore;after:tesla";
 	
 	public static Config config;

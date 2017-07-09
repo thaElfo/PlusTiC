2ff84451a2ffbd79b855283dc0b341da2c03ec14
Landmaster <python0429@gmail.com>
2017-07-08 20:13:42 -0400

    Fix #64

--- src/main/java/landmaster/plustic/util/PsiUtils.java
+++ src/main/java/landmaster/plustic/util/PsiUtils.java
@@ -6,7 +6,7 @@ import vazkii.psi.common.core.handler.*;
 
 public class PsiUtils {
 	public static int extractPsi(EntityPlayer player, int maxExtract) {
-		if (Loader.isModLoaded("Psi") && PlayerDataHandler.get(player) != null && PlayerDataHandler.get(player).getCAD() != null) {
+		if ((Loader.isModLoaded("Psi") || Loader.isModLoaded("psi")) && PlayerDataHandler.get(player) != null && PlayerDataHandler.get(player).getCAD() != null) {
 			int amount = PlayerDataHandler.get(player).getAvailablePsi();
 			PlayerDataHandler.get(player).deductPsi(Math.min(maxExtract, amount), 20, true);
 			return Math.min(maxExtract, amount);
@@ -15,7 +15,7 @@ public class PsiUtils {
 	}
 	
 	public static int extractPsiExact(EntityPlayer player, int extract) {
-		if (Loader.isModLoaded("Psi") && PlayerDataHandler.get(player) != null && PlayerDataHandler.get(player).getCAD() != null) {
+		if ((Loader.isModLoaded("Psi") || Loader.isModLoaded("psi")) && PlayerDataHandler.get(player) != null && PlayerDataHandler.get(player).getCAD() != null) {
 			int amount = PlayerDataHandler.get(player).getAvailablePsi();
 			if (amount >= extract) {
 				PlayerDataHandler.get(player).deductPsi(extract, 20, true);

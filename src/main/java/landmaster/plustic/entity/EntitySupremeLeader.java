2f7ab59b15e823a67e113630be24ccc3a1ba699d
Thanh-Phuong <python0429@gmail.com>
2017-07-20 11:17:56 -0400

    Fix #66

--- src/main/java/landmaster/plustic/entity/EntitySupremeLeader.java
+++ src/main/java/landmaster/plustic/entity/EntitySupremeLeader.java
@@ -99,7 +99,7 @@ public class EntitySupremeLeader extends EntityCreature {
 		this.tasks.addTask(1, new EntityAISwimming(this));
 		this.tasks.addTask(2, new EntityAISupremeLeaderSwell(this));
 		this.tasks.addTask(4, new EntityAIAttackMelee(this, 1.0D, false));
-		this.tasks.addTask(5, new EntityAIWanderAvoidWater(this, 0.8D));
+		this.tasks.addTask(5, new EntityAIWander(this, 0.8D));
 		this.tasks.addTask(6, new EntityAIWatchClosest(this, EntityPlayer.class, 8.0F));
 		this.tasks.addTask(6, new EntityAILookIdle(this));
 		this.targetTasks.addTask(1, new EntityAIHurtByTarget(this, false));

4de48facc3e6c338db98b6d27763bd5f7653eaf1
Thanh-Phuong <python0429@gmail.com>
2017-08-14 23:02:16 -0400

    Add Ghostwood and Fusewood; closes #93

---
+++ src/main/java/landmaster/plustic/traits/Ghastly.java
@@ -0,0 +1,33 @@
+package landmaster.plustic.traits;
+
+import java.util.*;
+
+import net.minecraft.entity.*;
+import net.minecraft.init.*;
+import net.minecraft.potion.*;
+import net.minecraft.util.*;
+import net.minecraftforge.common.*;
+import net.minecraftforge.event.entity.living.*;
+import net.minecraftforge.fml.common.eventhandler.*;
+import slimeknights.tconstruct.library.traits.*;
+
+public class Ghastly extends AbstractTrait {
+	public static final Ghastly ghastly = new Ghastly();
+	
+	public Ghastly() {
+		super("ghastly", 0xFFFFFF);
+		MinecraftForge.EVENT_BUS.register(this);
+	}
+	
+	@SubscribeEvent(priority = EventPriority.LOWEST)
+	public void defend(LivingHurtEvent event) {
+		if (event.getEntityLiving().isSneaking() && event.getSource() instanceof EntityDamageSource) {
+			Optional.ofNullable(event.getSource().getTrueSource()).ifPresent(attacker -> {
+				if (attacker instanceof EntityLivingBase) {
+					((EntityLivingBase)attacker).addPotionEffect(
+							new PotionEffect(MobEffects.SLOWNESS, 100, 2));
+				}
+			});
+		}
+	}
+}
